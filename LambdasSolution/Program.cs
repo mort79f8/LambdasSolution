66331e85459ea0fe1c62069e938ee80bdb46b2f7
mort79f8 <mort79f8@edu.campusvejle.dk>
2020-02-27 13:47:07 +0100

    Add project files.

---
+++ LambdasSolution/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdasSolution
+{
+    class Program
+    {
+
+        public delegate void myEventHandler(int value);
+
+        class PiggyBankEventHandler
+        {
+            private int total;
+
+            // declare the event
+            public event myEventHandler valueChanged;
+
+            public int Total
+            {
+                get => total;
+                set
+                {
+                    total = value;
+                    // when the value changes, fire the event
+                    valueChanged(total);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            PiggyBankEventHandler piggybank = new PiggyBankEventHandler();
+            piggybank.valueChanged += (x) =>
+            {
+                if (x > 500)
+                {
+                    Console.WriteLine("You have reached your savings goal!");
+                }
+                Console.WriteLine($"Total in the piggybank: {x}");
+            };
+
+            string str;
+            do
+            {
+                Console.WriteLine("Enter the amount you which to put in the bank:");
+                str = Console.ReadLine();
+                if (!str.Equals("exit"))
+                {
+                    piggybank.Total += int.Parse(str);
+                }
+            } while (!str.Equals("exit"));
+
+        }
+
+        //static void Piggybank_ValueChanged(int value)
+        //{
+        //    int savingsGoal = 500;
+        //    if (value >= savingsGoal)
+        //    {
+        //        Console.WriteLine("You have reach your savings goal!!");
+        //    }
+
+        //    Console.WriteLine($"Total in the piggybank: {value}");
+        //}
+    }
+}
